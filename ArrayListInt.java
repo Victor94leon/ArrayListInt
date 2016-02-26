536284aede91310eeeacdba4f777a0eb42ff144a
Víctor <victorlopez94leon@gmail.com>
2016-02-26 11:49:38 +0100

    Creada la clase ArrayListInt con un atributo y un método constructor

---
+++ ArrayListInt.java
@@ -0,0 +1,14 @@
+
+public class ArrayListInt
+{
+    // Array para almacenar números enteros
+    private int[] array;
+
+    /**
+     * Constructor de objetos de la clase ArrayListInt
+     */
+    public ArrayListInt()
+    {
+        array = new int[0];
+    }
+}
