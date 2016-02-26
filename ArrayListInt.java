5f472be6e75b159f61a6d5f16eee3cc60e37d5a2
Víctor <victorlopez94leon@gmail.com>
2016-02-26 13:32:56 +0100

    Añadido método remove para eliminar un elemento de la lista

--- ArrayListInt.java
+++ ArrayListInt.java
@@ -169,4 +169,39 @@ public class ArrayListInt
     {
         return array.length;
     }
+
+    /**
+     *  Elimina de la colección el elemento que ocupa la posición especificada y devuelve dicho elemento. 
+     *  Si el valor de index no es válido, no hace nada y devuelve -1.
+     */
+    public int remove(int index)
+    {
+        int elementoEliminado = -1;
+        if (index<array.length) {
+            // Se crea un nuevo array con el tamaño del atributo array decrementado en 1
+            int[] nuevoArray = new int[array.length - 1];
+            // Se crea un contador para recorrer los elementos de los arrays
+            int cont = 0;
+            // Mientras el ocntador sea menorque el indice se ejecutara un bucle para ir añadiendo nuevos elementos
+            while (cont<=index) {
+                // Si el contador es igual que el indice se añade el elemento en el indice y se hace un bucle para ir añadiendo
+                // los elementos de la antigua lista con el indice incrementado en 1.
+                if (cont==index) {
+                    elementoEliminado = array[index];
+                    cont++;
+                    while (cont<nuevoArray.length) {
+                        nuevoArray[cont-1] = array[cont-2];
+                        cont++;
+                    }
+                }
+                else {
+                    nuevoArray[cont] = array[cont];
+                    cont++;
+                }
+            }
+            // Se sustituye el atributo array por nuevoArray
+            array = nuevoArray;
+        }
+        return elementoEliminado;
+    }
 }
